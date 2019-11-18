d73623d90073f80045e11691104e15ee17af6787
Ubuntu <ubuntu@ip-172-31-86-204.ec2.internal>
2019-11-18 16:16:03 +0000

    new templates and styling

---
+++ run.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/learner")
+def learner():
+    return render_template("learner.html")
+    
+@app.route("/seller")
+def seller():
+    return render_template("seller.html")
+
+@app.route("/school")
+def school():
+    return render_template("school.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+    
+@app.route("/database")
+def database():
+    return render_template("database.html")
+    
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+            
+# Debug=true should be removed on submission!
