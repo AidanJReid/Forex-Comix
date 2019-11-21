55b693c4314437162ac33d3636afc05b1a7aaef7
Ubuntu <ubuntu@ip-172-31-86-204.ec2.internal>
2019-11-21 19:24:01 +0000

    additional templates, contact form, cards from dbase

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'something_stupid'
 
 @app.route('/')
 def index():
@@ -9,27 +11,46 @@ def index():
     
 @app.route('/learner')
 def learner():
-    return render_template("learner.html")
+    return render_template("learner.html", page_title="Learner")
     
-@app.route('/seller')
-def seller():
-    return render_template("seller.html")
+@app.route('/bookseller')
+def bookseller():
+    return render_template("bookseller.html", page_title="Bookseller")
 
 @app.route('/school')
 def school():
-    return render_template("school.html")
+    return render_template("school.html", page_title="School")
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", page_title="Login")
     
 @app.route('/database')
 def database():
-    return render_template("database.html")
+    data = []
+    with open("data/comics.json","r") as json_data:
+        data = json.load(json_data)
+    return render_template("database.html", page_title="Database", comics=data)
     
-@app.route('/contact')
+@app.route('/database/<comic_title>')
+def database_comic(comic_title):
+    comic = {}
+    
+    with open("data/comics.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == comic_title:
+                comic = obj
+                
+    return render_template("comic.html", comic=comic)
+                
+    
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        flash("Thanks {}, I got your message, loud and clear!".format(
+              request.form["name"]))
+    return render_template("contact.html", page_title="Contact")
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
