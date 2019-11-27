77c9ff99fc4a03ee19b6eb6a14228dacc2e59afa
Ubuntu <ubuntu@ip-172-31-86-204.ec2.internal>
2019-11-27 16:22:27 +0000

    adding enabled for comics

--- run.py
+++ run.py
@@ -13,43 +13,51 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
     
 @app.route('/learner')
 def learner():
-    return render_template("learner.html", page_title="Learner")
+    return render_template('learner.html', page_title='Learner')
     
 @app.route('/bookseller')
 def bookseller():
-    return render_template("bookseller.html", page_title="Bookseller")
+    return render_template('bookseller.html', page_title='Bookseller')
 
 @app.route('/school')
 def school():
-    return render_template("school.html", page_title="School")
+    return render_template('school.html', page_title='School')
 
 @app.route('/login')
 def login():
-    return render_template("login.html", page_title="Login")
+    return render_template('login.html', page_title='Login')
     
 @app.route('/addcomic')
 def addcomic():
-    return render_template("addcomic.html", page_title="Add Comic", languages=mongo.db.Languages.find())
+    return render_template('addcomic.html', page_title='Add Comic', 
+    languages=mongo.db.Languages.find(),
+    owner=mongo.db.owner.find())
     
-@app.route('/database', methods=["GET"])
+@app.route('/insert_comic', methods=['POST'])
+def insert_comic():
+    DBComix = mongo.db.DBComix
+    DBComix.insert_one(request.form.to_dict())
+    return redirect(url_for('database'))
+    
+@app.route('/database', methods=['GET'])
 def database():
-    return render_template("database.html", page_title="Database", DBComix=mongo.db.DBComix.find())
+    return render_template('database.html', page_title='Database', DBComix=mongo.db.DBComix.find())
     
-@app.route('/database/<comic_title>')
+@app.route('/database/<comic_title>', methods=['GET'])
 def database_comic(comic_title):
-    return render_template("comic.html", DBComix=mongo.db.DBComix.find())
+    return render_template('comic.html', DBComix=mongo.db.DBComix.findOne())
                 
     
-@app.route('/contact', methods=["GET", "POST"])
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == "POST":
+    if request.method == 'POST':
         flash("Thanks {}, I got your message, loud and clear!".format(
-              request.form["name"]))
-    return render_template("contact.html", page_title="Contact")
+              request.form['name']))
+    return render_template('contact.html', page_title='Contact')
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
